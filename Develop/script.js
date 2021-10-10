b06b89427500cb9663c1d18b39293a4c1e9025d1
pillow <fard.cca@gmail.com>
2021-10-10 14:56:31 +0200

    Fix the code, add readme file

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,115 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-// Input Object
-
+// Input Object; Define all variables and their values that we need for securepassword
 var input = {
-  letter : ["a", "b", "c", "d", "e", "f"],
-  letterUpper : ["A","B","C","D","E"],
-  number : [1, 3, 6, 9, 7, 8],
-  char : ["#", "%", ",","?"]
+  letter : ["a", "b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r",
+  "s","t","u","v","w","x","y","z"],
+
+  letterUpper : ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+  "S","T","U","V","W","X","Y","Z"],
+
+  number : ["1","2","3","4","5","6","7","8","9","0"],
+
+  char : ["#", "%", "@","?", "|","/","$","_","-"],
 }
+// //Random Function; Define assisstant-function to call later to generate different characters
 function randomSel(arr){
   var index = Math.floor(Math.random() * arr.length);
   return index;
 }
-  // Write password to the #password input
+// //Function generatePassword; Prompt the inputs and generate the password
 function generatePassword() {
+  // Prompt: prompt the machine the type of characters include in password
 
-  var passLetter = input.letter[randomSel(input.letter)];
-  var passLetterUpper = input.letterUpper[randomSel(input.letterUpper)];
-  var passNumber = input.number[randomSel(input.number)];
-  var passCharacter = input.char[randomSel(input.char)];
-  var securePass = (passLetter + passLetterUpper + passNumber + passCharacter);
-  return securePass
+  window.alert("please start password generation your password should contain at least one of the coming types");
+  var smallLetterInclude = window.prompt("Enter   s   if you like your password include small letters");
+  var capitalLetterInclude = window.prompt("Enter   c   if you like your password include capital letters");
+  var numberInclude = window.prompt("Enter   n   if you like your password include numbers");
+  var characterInclude = window.prompt("Enter   g   if you like your password include special characters");
+  // While loop: check if the needed tyes are in hand or not, if not continue to message the user
+  while (smallLetterInclude != "s" && capitalLetterInclude != "c" && numberInclude != "n" && characterInclude != "g"){
+    window.alert("at least one of the types should be selected")
+    var smallLetterInclude = window.prompt("Enter   s   if you like your password include small letters");
+    var capitalLetterInclude = window.prompt("Enter   c   if you like your password include capital letters");
+    var numberInclude = window.prompt("Enter   n   if you like your password include numbers");
+    var characterInclude = window.prompt("Enter   g   if you like your password include special characters");
+
+  }
+  var numberOfCharacter = window.prompt("Enter the number of characters of password between 8 and 128");
+  console.log(numberOfCharacter);
+  var newNumber = parseInt(numberOfCharacter);
+  // While loop: check if the number of the characters of the password are between 8 and 128 or not, if not message the user to input the right number
+  while (newNumber < 8 || newNumber > 128){
+    window.alert("the number of characters of the password should be between  8 and 128");
+    var numberOfCharacter = window.prompt("Enter the number of characters of password between 8 and 128");
+    console.log(numberOfCharacter);
+    var newNumber = parseInt(numberOfCharacter);
+
+  }
+  console.log(typeof(newNumber));
+  var securePass =[];
+  count = 0;
+  for (i = 0; i <= newNumber; i = count) {
+    if (count > newNumber) {
+        break;
+      }
+    if(smallLetterInclude){
+      var smallLetter = input.letter[randomSel(input.letter)];
+      count = count+1;}
+      else {
+        var smallLetter = "";
+      }
+      securePass.push(smallLetter);
+
+      
+    if (count >= newNumber) {
+        break;
+      }
+    if(capitalLetterInclude){
+      var capitalLetter = input.letterUpper[randomSel(input.letterUpper)];
+      count = count+1;}
+      else {
+        var capitalLetter = "";
+      }
+      securePass.push(capitalLetter);
+
+
+    if (count >= newNumber) {
+      break;
+      }
+    if(numberInclude){
+      var num = input.number[randomSel(input.number)];
+      count = count+1;}
+      else {
+        var num = "";
+      }   
+      securePass.push(num);
+
+
+    if (count >= newNumber) {
+      break;
+      }
+    if (characterInclude){
+      var character = input.char[randomSel(input.char)];
+      count = count+1;}
+      else {
+        var character = "";
+      }      
+      securePass.push(character);
+    // securePass.push(smallLetter,capitalLetter,num,character);
+  
+  }
+  char = ""
+  for (i = 0 ; i < securePass.length ; i ++){
+      var char = char + securePass[i];
+  }
+
+  return char;
 
 };
-
-// ___________________________________
- function writePassword() {
+// //Function writePassword; call generatePassword and put it in the #password input
+function writePassword() {
   // ___________________________________
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -35,6 +117,5 @@ function generatePassword() {
   passwordText.value = password;
 
 };
-
-// Add event listener to generate button
+// // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,92 +1,16 @@
-# 03 JavaScript: Password Generator
+bootCampBerlin - 03-Homework - 10,10,201
 
-## Your Task
+titleOfTheProject: JavaScript - PasswordGenerator
 
-This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+containing files: index.html, script.js, README.md
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+task: generate a password, using input prompts from the user. the password should be secure and generate automatically by machine. the user shall only declare the type of the characters and the number of them according to the criteria policies.
 
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-
-## Mock-Up
-
-The following image shows the web application's appearance and functionality:
-
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
-
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria plus the following:
-
-  * The homework should not produce any errors in the console when you inspect it using Chrome DevTools.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+process: 
+1- build a repository in github
+2- clone the repository in local computer
+3- draw the flowchart(layout of the project) to simplify the process. this flowchart contains inputs(prompt), the body which consists of two main functions and the output (eventlistener)
+4- define the first structure of the code using two main functions, generator and reader in script.js
+5- develop the code, add check lines for right type of characters
+6- control the code if the wrong input will result in collaps
+7- add, commit push to github
